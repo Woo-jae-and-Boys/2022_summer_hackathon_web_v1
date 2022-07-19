1539e698a152bf8e4b0edbc445456c936838bde8
nrbns357 <gusgh357@gmail.com>
2022-07-19 14:42:18 +0900

    Add: detail page 구현

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Main from "./components/main/main";
+import DetailPage from "./components/detailPage/detailPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/detailPage" element={<DetailPage />} />
       </Routes>
     </BrowserRouter>
   );
