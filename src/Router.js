add421824066ad2ddcdb8100ea9131cb3361fc37
kimyerim01118 <72791856+kimyerim01118@users.noreply.github.com>
2022-07-19 16:33:26 +0900

    add: 회원가입

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Main from "./components/main/main";
+import SignUp from "./components/signUp/signUp";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="signUp" element={<SignUp/>}/>
       </Routes>
     </BrowserRouter>
   );
