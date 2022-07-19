0830ed43403b077171f13c92a79c399471792387
nrbns357 <gusgh357@gmail.com>
2022-07-19 11:19:58 +0900

    Fix : 충돌 방지를 위한 코드 삭제

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -2,19 +2,7 @@ import React, { useState } from "react";
 import DropDown from "../common/dropDown/dropDown";
 
 const Main = () => {
-  const [selectedValue, setSeletedValue] = useState("");
-  const dropDownChildValue = ["정우재", "금현호", "우리팀 화이팅"];
-
-  console.log(selectedValue);
-  return (
-    <>
-      <DropDown
-        defaultValue={"사람"}
-        childValue={dropDownChildValue}
-        setSeletedData={setSeletedValue}
-      />
-    </>
-  );
+  return <></>;
 };
 
 export default Main;
