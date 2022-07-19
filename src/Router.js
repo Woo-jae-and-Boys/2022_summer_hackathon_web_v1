98cd85613a5afe81b854835047e9241d6fbfeee5
전해림 <101709981+HAERIM0@users.noreply.github.com>
2022-07-19 15:01:47 +0900

    Add: Login 구현

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Main from "./components/main/main";
+import Login from "./components/login/login";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
